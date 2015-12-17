03b658851dc3170f08fd998c87ef3d341b85f0f1
shgfl007 <shgfl@hotmail.com>
2015-12-16 23:12:18 -0500

    vid background added

--- Test/www/js/index_user_scripts.js
+++ Test/www/js/index_user_scripts.js
@@ -8,6 +8,12 @@
  {
     
     
- }
+     /* button  .uib_w_28 */
+    $(document).on("click", ".uib_w_28", function(evt)
+    {
+         activate_page("#scene_1"); 
+    });
+    
+    }
  document.addEventListener("app.Ready", register_event_handlers, false);
 })();
